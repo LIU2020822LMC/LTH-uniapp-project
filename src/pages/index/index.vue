01dd8fcdfcb071c95273c606be756a44c3bb26c0
LiMingCong <2058615288@qq.com>
2026-04-27 23:45:23 +0800

    feat: 前台分类-组件封装

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import CustomNavbar from './components/CustomNavbar.vue'
+import CategoryPanel from './components/CategoryPanel.vue'
 import { getHomeBannerAPI } from '@/services/home'
 import type { BannerItem } from '@/types/home'
 import { onLoad } from '@dcloudio/uni-app'
@@ -21,7 +22,12 @@ onLoad(() => {
 <template>
   <CustomNavbar />
   <LthSwiper :list="bannerList" />
+  <CategoryPanel />
   <view class="index">index</view>
 </template>
 
-<style lang="scss"></style>
+<style lang="scss">
+page {
+  background-color: #f7f7f7;
+}
+</style>
